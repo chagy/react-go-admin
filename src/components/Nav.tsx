50de5ea50ec7b2eea63553ab89d22d5c39075240
chagy <chagy_x_@hotmail.com>
2021-04-28 11:04:26 +0700

    useEffect

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,35 +1,41 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Nav = () => {
+  const [user,setUser] = useState({
+    first_name: ''
+  })
+
+  useEffect(() => {
+    (
+      async() => {
+        const { data } = await axios.get(`http://localhost:8000/api/user`,{withCredentials:true});
+        setUser(data);
+      }
+    )();
+  }, [])
+
   return (
     <>
       <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
         <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
           Company name
         </a>
-        <button
-          className="navbar-toggler position-absolute d-md-none collapsed"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#sidebarMenu"
-          aria-controls="sidebarMenu"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span className="navbar-toggler-icon" />
-        </button>
-        <input
-          className="form-control form-control-dark w-100"
-          type="text"
-          placeholder="Search"
-          aria-label="Search"
-        />
         <ul className="navbar-nav px-3">
-          <li className="nav-item text-nowrap">
-            <a className="nav-link" href="#">
-              Sign out
-            </a>
-          </li>
+          {user ? (
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                {user.first_name} Sign out
+              </a>
+            </li>
+          ) : (
+            <li className="nav-item text-nowrap">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+          )}
+          
         </ul>
       </header>
     </>
