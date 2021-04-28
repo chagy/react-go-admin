260e3891395fda33b935a2dea672e0f72a561361
chagy <chagy_x_@hotmail.com>
2021-04-28 10:51:44 +0700

    Login

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Dashboard from "./pages/Dashboard";
 import Users from "./pages/Users";
 import { BrowserRouter, Route } from "react-router-dom";
 import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -14,7 +15,7 @@ function App() {
         <Route path={"/"} exact component={Dashboard} />
         <Route path={"/users"} component={Users} />
         <Route path={"/register"} component={Register} />
-        
+        <Route path={"/login"} component={Login} />
       </BrowserRouter>
     </div>
   );
